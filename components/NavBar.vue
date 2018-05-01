1eea225269158b7a19e5b8d51cf591854d1d36be
Oliver Bruehl <oliver.bruehl@gmail.com>
2018-05-01 11:53:58 +0200

    add navbar component

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav>
+      <ul>
+      <li>
+        <router-link :to="{ path: this.$store.state.locale + '/' }">{{ $t('home.title') }}</router-link>
+      </li>
+      <li>
+        <router-link :to="{ path: this.$store.state.locale + '/news' }">{{ $t('news.title') }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+
+</style>
--- pages/_lang/index.vue
+++ pages/_lang/index.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="Content">
     <div class="container">
+      <NavBar/>
+    </div>
+    <div class="container">
       <h1 class="Content__Title">{{ $t('home.title') }}</h1>
       <p>{{ $t('home.introduction') }}</p>
       <router-link :to="{ path: this.$store.state.locale + '/news' }">{{ $t('news.title') }}</router-link>
     </div>
   </div>
 </template>
+
 <script>
-export default {
-  head() {
-    return { title: this.$t('home.title') }
+  import NavBar from '~/components/NavBar.vue'
+
+  export default {
+    components: {
+      NavBar
+    },
+    head() {
+      return { title: this.$t('home.title') }
+    }
   }
-}
 </script>
